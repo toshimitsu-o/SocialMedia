76007a414437784cfcefeec5bb2798957bbf190e
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-08-20 11:17:39 +1000

    Add database setup sql file

---
+++ laravel/database/init_set_up.sql
@@ -0,0 +1,51 @@
+-- Enable foreign key
+PRAGMA foreign_keys = ON;
+
+-- Creation of tables
+DROP TABLE IF EXISTS Like;
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User (
+    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE Post (
+    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Title VARCHAR(120) NOT NULL,
+    Author INTEGER NOT NULL REFERENCES User(Id),
+    Message TEXT NOT NULL,
+    Date TIMESTAMP
+);
+
+CREATE TABLE Comment (
+    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    PostId TINTEGER NOT NULL REFERENCES Post(Id),
+    Author INTEGER NOT NULL REFERENCES User(Id),
+    Message TEXT NOT NULL,
+    Date TIMESTAMP,
+    ReplyTo INTEGER REFERENCES Comment(Id)
+);
+
+CREATE TABLE Like (
+    PostId INTEGER NOT NULL REFERENCES Post(Id),
+    User INTEGER NOT NULL REFERENCES User(Id)
+);
+
+-- Inserting sample data
+
+INSERT INTO User VALUES (NULL, "UserA");
+INSERT INTO User VALUES (NULL, "UserB");
+INSERT INTO User VALUES (NULL, "UserC");
+
+INSERT INTO Post VALUES (NULL, "Post Test A", 1, "Sample message for the post A.", "2023-08-18 10:00:00");
+INSERT INTO Post VALUES (NULL, "Post Test B", 2, "Sample message for the post B.", "2023-08-19 12:00:00");
+
+INSERT INTO Comment VALUES (NULL, 1, 2, "This is a test comment.", "2023-08-20 10:00:00");
+INSERT INTO Comment VALUES (NULL, 1, 1, "This is a test reply to a comment.", "2023-08-21 14:00:00", 1);
+
+INSERT INTO Like VALUES (1, 2);
+INSERT INTO Like VALUES (1, 3);
+INSERT INTO Like VALUES (2, 1);
